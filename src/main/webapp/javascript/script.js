7ed5e6d7df443b27ac2a5cc0f649a9e1a0c75f83
Zain Ul Abidin <Zain Ul Abidin@DESKTOP-FB9EBS7>
2026-04-21 15:18:59 +0500

    script file small changes

--- src/main/webapp/javascript/script.js
+++ src/main/webapp/javascript/script.js
@@ -1,8 +1,5 @@
-
-//Initialize AOS (Animate On Scroll)
 AOS.init();
 
-//Show success message if URL contains ?success=true
 const params=new URLSearchParams(window.location.search);
 if(params.get("success")==="true"){
 const msg=document.getElementById("successMsg");
